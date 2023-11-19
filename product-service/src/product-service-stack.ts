0ff50c7a3da4203eb14ceefd60daf283b2914b77
Anna Tolstova <anna_tolstova@hotmail.com>
2023-11-19 18:20:38 +0200

    init: add product service stack

---
+++ product-service/src/product-service-stack.ts
@@ -0,0 +1,11 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+const bucketName = "anntol-cdk23";
+
+export class ProductServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+  }
+}
